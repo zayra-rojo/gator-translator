1056a50fc897707d650736de28f8006d9d98315f
Zayra Rojo <rojozayra@gmail.com@users.noreply.github.com>
2021-03-10 20:41:21 -0500

    added dialogue boxes, text box, and send button

--- src/components/BackgroundCard.js
+++ src/components/BackgroundCard.js
@@ -4,9 +4,11 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import Figure from "react-bootstrap/Figure";
+import DialogueCard from "./DialogueCard";
+import Form from "react-bootstrap/Form";
 import "./styles.css";
 
-const figureStyle = {
+const figureStyleBackground = {
   width: "500px",
   height: "600px",
   borderRadius: "16px",
@@ -15,11 +17,35 @@ const figureStyle = {
   boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
   backgroundColor: "rgb(209, 230, 224)",
 };
+const figureStyleChat = {
+  // width: "500px",
+  // height: "600px",
+  borderRadius: "16px",
+  marginRight: "24px",
+  padding: "20px 40px",
+  boxShadow: "5px 8px 24px 5px rgba(208, 216, 243, 0.6)",
+  backgroundColor: "white",
+};
+let guideCardContent = "Hi, do you need directions?";
+let internationalCardContent = "Yes, how do i get to Chomp It?";
 function BackgroundCard() {
   return (
-    <Figure style={figureStyle}>
+    <Figure style={figureStyleBackground}>
       <h2>International Student</h2>
       <h4>Current language: Spanish</h4>
+      <Figure style={figureStyleChat}>
+        <DialogueCard user="guide" content={guideCardContent} />
+        <DialogueCard user="international" content={internationalCardContent} />
+      </Figure>
+      <Form>
+        <Form.Group controlId="exampleForm.ControlTextarea1">
+          <Form.Label>Example textarea</Form.Label>
+          <Form.Control as="textarea" rows={3} />
+        </Form.Group>
+        <Button variant="primary" type="submit">
+          Send
+        </Button>
+      </Form>
     </Figure>
   );
 }
